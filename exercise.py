3b03fde4832b0161c24ee4ff06c0c633e23bf58a
Tim Long <cooder45@gmail.com>
2017-08-17 09:45:30 +0100

    17th aud

---
+++ exercise.py
@@ -0,0 +1,33 @@
+
+# 1. This is the print statement
+#print("Hello world")
+
+# 2. This is a variable and command to print variable (this varaible is a string)
+#message = "Level Two"
+#print (message)
+
+#3 You can ask Python what type a variable is.
+#print(type(message))
+
+
+# 4. Another type of variable is an integer (a whole number)
+a = 123
+b = 654
+c = a + b
+#print (c)
+
+
+# 5. You can use other operators like subtract (-) and multiply (*)
+
+
+
+
+# 6. Variables keep their value until you change it
+a= 100
+print(a)
+
+c=50
+print(c)
+
+d = 10 + a - c
+print (d)
