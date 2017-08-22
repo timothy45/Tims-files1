601ed3b87e9df3a798fd00c7b648dab287eb1d0a
Tim Long <cooder45@gmail.com>
2017-08-22 09:54:23 +0100

    ssnd aug

--- Python training/exercise.py
+++ Python training/exercise.py
@@ -43,8 +43,71 @@
 age =  65
 name = 'Tim'
 #print(name + ' is ' + age + ' years old') this produces error. correct shown below
-print(name + ' is ' + str(age) + ' years old')
+
+# 9. We can convert numbers to strings like this:
+#print(name + ' is ' + str(age) + ' years old')
+
+# 10. Another variable type is called a boolean
+# This means either True or False
+raspberry_pi_is_fun = True
+raspberry_pi_is_expensive = False
+#= is an assignment operator
+#== is an equality operator
+#bobs_age = 14
+#your_age = 15
+#print(your_age == bobs_age)  # this prints either True or False
 
 
+
+# 11. We can use less than and greater than too - these are < and >
+#bob_is_older = bobs_age > your_age
+#print(bob_is_older)  # do you expect True or False?
+
+# 12. We can ask questions before printing with an if statement
+#money = 500
+#phone_cost = 201
+#tablet_cost = 300
+#total_cost = phone_cost + tablet_cost
+#can_afford_both = money >= total_cost
+#if can_afford_both:
+ #   message = "You have enough money for both"
+#else:
+  #  message = "You can't afford both devices"
+#print(message)  # what do you expect to see here?
+
+
+  
+
+# 13. You can keep many items in a type of variable called a list
+#colours = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+# You can check whether a colour is in the list
+#print('Black' in colours)  # Prints True or False
+# You can add to the list with append
+#colours.append('Black')
+#colours.append('White')
+#print('Black' in colours)  # Prints True or False
+#print (colours)
+
+
+# 14. You can add two lists together in to a new list using +
+
+primary_colours = ['Red', 'Blue', 'Yellow']
+
+secondary_colours = ['Purple', 'Orange', 'Green']
+
+#main_colours = primary_colours + secondary_colours
+
+
+#print (main_colours)
+
+
+
+# 15. You can find how many there are by using len(your_list). Try it below
+
+# How many colours are there in main_colours?
+
+len(primary_colours)
+
+# GO!
 
 
